696a92443dd2735810309160330692884f8aeddc
codewithdary <info@darynazar.com>
2023-02-21 16:48:11 +0100

    Updated Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\Post;
 use App\Models\Tag;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,6 +17,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        User::factory()->times(10)->create();
+        Post::factory()->times(20)->create();
+        Tag::factory()->times(4)->create();
+
         for($i = 0; $i < 20; $i++) {
             \App\Models\PostTag::factory()->create([
                 'post_id' => Post::all()->random()->id,
